892f68d238cc13816657648a6e66c8f986ea879a
Lars Oskarsen <lars@auka.io>
2018-10-02 15:08:12 +0200

    Wrap urlfetch.DownloadError in MapiError

--- mcash/mapi_client/backends/urlfetch.py
+++ mcash/mapi_client/backends/urlfetch.py
@@ -1,5 +1,7 @@
 from ..mapi_response import MapiResponse
+from ..mapi_error import MapiError
 import json
+import logging
 
 from google.appengine.api import urlfetch
 
@@ -17,10 +19,17 @@ class UrlFetchFramework(object):
                     payload.update({key: value})
             data = json.dumps(payload)
 
-        res = urlfetch.fetch(url=url,
-                             payload=data,
-                             method=method,
-                             deadline=60,
-                             follow_redirects=False,
-                             headers=headers)
-        return MapiResponse(res.status_code, res.headers, res.content)
+        try:
+            res = urlfetch.fetch(url=url,
+                                payload=data,
+                                method=method,
+                                deadline=60,
+                                follow_redirects=False,
+                                headers=headers)
+            return MapiResponse(res.status_code, res.headers, res.content)
+        except urlfetch.DownloadError as e:
+            # Wrap in MapiError -- client code shouldn't have to
+            # worry about URLFetch-specific exceptions
+            logging.error('Unable to fetch %s', url)
+            logging.error('%r', e)
+            raise MapiError(None, None, None)
